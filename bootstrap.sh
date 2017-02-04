35cc4ce3e68143d39892e874854098e6f05961ea
Christian-Manuel Butzke <chris@fruwe.com>
2017-02-05 02:42:04 +0900

    add rubygems update remove unused packages allow bootstrap without git pull

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,19 @@ set -e
 
 # Download and install Command Line Tools if no developer tools exist
 if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]]; then
-    echo "Info   | Install   | xcode"
+    echo "Info     | Install     | xcode"
     xcode-select --install
 fi
 
 # Download and install Homebrew
 if [[ ! -x /usr/local/bin/brew ]]; then
-    echo "Info   | Install   | homebrew"
+    echo "Info     | Install     | homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Download and install Ansible
 if [[ ! -x /usr/local/bin/ansible ]]; then
-    echo "Info   | Install   | Ansible"
+    echo "Info     | Install     | Ansible"
     brew update
     brew install ansible
 fi
@@ -27,7 +27,10 @@ fi
 # This should be subsequently updated in shell settings
 export PATH=/usr/local/bin:$PATH
 
-git pull
+if [[ -d .git ]]; then
+    echo "Info     | mac-ansible | Pull"
+    git pull
+fi
 
 # Use this if you use the vault
 # if [[ ! -f ~/.ansible_vault_pass.py ]]; then
